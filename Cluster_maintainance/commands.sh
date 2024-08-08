313bac61a5c45e7d9e5660c55ae80b1150b08306
arka <arkashankarpradhan@gmail.com>
2024-08-08 18:27:38 +0530

    added commands for update in master and worker node

--- Cluster_maintainance/commands.sh
+++ Cluster_maintainance/commands.sh
@@ -14,3 +14,21 @@ kubeadm upgrade apply
 
 #command to update the kubeadm
 apt-get upgrade -y kubeadm = 1.12.0-00
+
+#next is command to apply those changes using apply for the master
+kubeadm upgrade apply v1.12.0
+
+#next is to update kubelet 
+apt-get upgrade -y kubelet = 1.12.0-00
+
+#once the package is upgraded, restart the kubelet service
+systemctl restart kubelet
+
+# next is to update the worker node
+kubectl drain node-1
+
+apt-get upgrade -y kubeadm = 1.12.0-00
+
+apt-get upgrade -y kubelet = 1.12.0-00
+
+kubeadm upgrade node config --kubelet-version v1.12.0
