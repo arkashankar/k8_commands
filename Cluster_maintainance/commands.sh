ee6cbcd51b7992f57424ebe21752d3cbaed3b627
arka <arkashankarpradhan@gmail.com>
2024-08-08 18:14:14 +0530

    added commands for kubeadm

--- Cluster_maintainance/commands.sh
+++ Cluster_maintainance/commands.sh
@@ -6,3 +6,11 @@ kubectl uncordon node-1
 
 #to make a node unschedulable
 kuebctl cordon node-1
+
+#version update info for the kubeadm 
+kubeadm upgrade plan
+
+kubeadm upgrade apply
+
+#command to update the kubeadm
+apt-get upgrade -y kubeadm = 1.12.0-00
---
+++ rolling_update/commands.sh
@@ -0,0 +1,5 @@
+#To set the image in the deployment
+kubectl set image deploy <deployment_name> <container_name>
+
+#to edit deployment
+kubectl edit deploy <deployment_name>
